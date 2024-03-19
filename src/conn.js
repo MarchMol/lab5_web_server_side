b70e5fa374d0a04ab4ce381d9ef3ae06126dcf40
MarchMol <marchmol98@gmail.com>
2024-03-18 19:13:04 -0600

    FIX: database name updated for the api

--- src/conn.js
+++ src/conn.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'marchena',
-  database: 'blog_db',
+  database: 'blog_marchena',
   password: '1234',
   waitForConnections: true,
   connectionLimit: 10,
