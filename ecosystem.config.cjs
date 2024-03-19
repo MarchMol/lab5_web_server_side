d3df2fa2829c33651a90b9071a90bd65220e742d
MarchMol <marchmol98@gmail.com>
2024-03-18 18:23:25 -0600

    FEAT: config file for pm2 for the next lab

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps:[{
+        name: 'blog marchena',
+        script: 'npm',
+        args: 'start',
+        log_file: './blog_marchena.txt',
+        merge_logs: true
+    }]
+}
